af50b5df42127be8428982521a611fe9fa3f7e7f
MABODO Eltone <eltone.mabodo@wfp.org>
2024-09-02 14:43:55 +0700

    Read in the data and created data for only wet commodities

--- code/script.R
+++ code/script.R
@@ -1,1 +1,11 @@
 library(tidyverse)
+library(readxl)
+
+
+SupplierData <- read_excel("data/IRL_Evaluation_of_Procurement_Piloting_Model_of_SF_program_Deidentified_Label.xlsx") %>% 
+  filter(!is.na(SbjNum))
+
+
+WetCommoditiesSup <- SupplierData %>% 
+  filter(ProductsSupplied_2 == "2. Wet Commodities [Vegetables, animal protein etc.]") %>% 
+  select(-c(I1_SupplyFreq:I1_OtherCostsMN))
