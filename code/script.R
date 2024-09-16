cdc0ad5cf23875b83776f3f18994913a68cdb3ef
MABODO Eltone <eltone.mabodo@wfp.org>
2024-09-16 11:11:10 +0700

    Wrote code to import and merge data for the entire study period, i.e., January 2023 - June 2024

--- code/script.R
+++ code/script.R
@@ -1,11 +1,274 @@
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
 
-SupplierData <- read_excel("data/IRL_Evaluation_of_Procurement_Piloting_Model_of_SF_program_Deidentified_Label.xlsx") %>% 
-  filter(!is.na(SbjNum))
+################################################################################
+# Load data
+
+#1. January 2023
+
+January_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                           sheet = "January 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "January",
+         Year = 2023)
 
 
-WetCommoditiesSup <- SupplierData %>% 
-  filter(ProductsSupplied_2 == "2. Wet Commodities [Vegetables, animal protein etc.]") %>% 
-  select(-c(I1_SupplyFreq:I1_OtherCostsMN))
+# 2. February 2023
+February_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                            sheet = "February 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "February",
+         Year = 2023)
+
+# 3. March 2023
+March_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                         sheet = "March 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "March",
+         Year = 2023)
+
+# 4. April 2023
+April_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                         sheet = "April 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "April",
+         Year = 2023)
+
+
+# 5. May 2023
+
+May_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                       sheet = "May 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "May",
+         Year = 2023)
+
+
+# 6. June 2023
+
+June_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                        sheet = "June 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "June",
+         Year = 2023)
+
+
+# 7. July 2023
+
+July_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                        sheet = "July 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "July",
+         Year = 2023)
+
+# 8. August 2023
+
+August_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                          sheet = "August 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "August",
+         Year = 2023)
+
+
+# 9. September 2023
+
+September_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                             sheet = "September 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "September",
+         Year = 2023)
+
+# 10. October 2023
+
+October_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                           sheet = "October 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "October",
+         Year = 2023)
+
+
+# November 2023
+
+November_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                            sheet = "November 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "November",
+         Year = 2023)
+
+# December 2023
+
+December_2023 <- read_excel("data/SFIS Clean_January_Dec_2023.xlsx",
+                            sheet = "December 2023") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "December",
+         Year = 2023)
+
+
+# Combine all the data
+Jan_Dec_2023_Data <- bind_rows(January_2023, 
+                               February_2023, 
+                               March_2023, 
+                               April_2023, 
+                               May_2023, 
+                               June_2023, 
+                               July_2023, 
+                               August_2023, 
+                               September_2023, 
+                               October_2023, 
+                               November_2023, 
+                               December_2023) %>% 
+  mutate(MonthYear = my(paste(Month, Year)),
+         MonthYear = format(MonthYear, "%b %Y"),
+         MonthYear = factor(MonthYear,
+                            levels = c("Jan 2023",
+                                       "Feb 2023",
+                                       "Mar 2023",
+                                       "Apr 2023",
+                                       "May 2023",
+                                       "Jun 2023",
+                                       "Jul 2023",
+                                       "Aug 2023",
+                                       "Sep 2023",
+                                       "Oct 2023",
+                                       "Nov 2023",
+                                       "Dec 2023"))) %>% 
+  # mutate the pilot schools varaible
+  mutate(PilotSchools = case_when(
+    District == "Phnum Kravanh" | District == "Ta Lou SenChey" ~ "Pilot School",
+    TRUE ~ "Non-Pilot School"))
+
+
+# load 2024 data
+
+January_2024 <- read_excel("data/SFIS Clean_January_June_2024.xlsx",
+                           sheet = "January 2024")%>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "January",
+         Year = 2024)
+
+
+February_2024 <- read_excel("data/SFIS Clean_January_June_2024.xlsx",
+                            sheet  = "February 2024") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "February",
+         Year = 2024)
+
+
+March_2024 <- read_excel("data/SFIS Clean_January_June_2024.xlsx",
+                         sheet = "March 2024") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "March",
+         Year = 2024)
+
+
+April_2024 <-  read_excel("data/SFIS Clean_January_June_2024.xlsx",
+                          sheet = "April 2024") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "April",
+         Year = 2024) 
+
+May_2024 <- read_excel("data/SFIS Clean_January_June_2024.xlsx",
+                       sheet = "May 2024") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "May",
+         Year = 2024) 
+
+
+June_2024 <- read_excel("data/SFIS Clean_January_June_2024.xlsx",
+                        sheet = "June 2024") %>% 
+  rename(AvgStudents = `​ AvgStudents`) %>% 
+  # Change character variables to factor
+  mutate(across(where(is.character), as.factor),
+         SchoolId = as.character(SchoolId)) %>% 
+  mutate(Month = "June",
+         Year = 2024) 
+
+
+# Compine the 2024 data tables
+
+Jan_June_2024 <- rbind(January_2024, February_2024,
+                       March_2024, April_2024,
+                       May_2024, June_2024) %>% 
+  mutate(MonthYear = my(paste(Month, Year)),
+         MonthYear = format(MonthYear, "%b %Y"),
+         MonthYear = factor(MonthYear,
+                            levels = c("Jan 2024",
+                                       "Feb 2024",
+                                       "Mar 2024",
+                                       "Apr 2024",
+                                       "May 2024",
+                                       "Jun 2024"))) %>% 
+  # mutate the pilot schools varaible
+  mutate(PilotSchools = case_when(
+    District == "Phnum Kravanh" | District == "Ta Lou SenChey" ~ "Pilot School",
+    TRUE ~ "Non-Pilot School"))
+  
+
+# Combine the two years
+
+FullTablesData <- rbind(Jan_Dec_2023_Data,
+                        Jan_June_2024)
+
+
+
+
+
+
+
+
+
+
+
