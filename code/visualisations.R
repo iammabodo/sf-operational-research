ceb1db2a2a436ad44a85a852352abf7b30e00cf6
MABODO Eltone <eltone.mabodo@wfp.org>
2024-11-03 13:41:40 +0700

    Suppliers costs and cost effcient graphs completed. The cost trend graph is still to be completed

--- code/visualisations.R
+++ code/visualisations.R
@@ -289,8 +289,8 @@ ggsave(
 
 
 costs_trend_graph <- PanelData %>% 
-  #filter(!MonthYear %in% c("Jan 2023", "Apr 2023", "May 2023", "Oct 2023", "Nov 2023", "Apr 2024", "Jul 2024")) %>%
-  ggplot(aes(x = MonthYear, y = AvgWetCostsPerChild, group = procurement, color = procurement)) +
+  filter(!MonthYear %in% c("Jan 2023", "Apr 2023", "May 2023", "Oct 2023", "Apr 2024")) %>%
+  ggplot(aes(x = MonthYear, y = AvgTotalCost, group = procurement, color = procurement)) +
   geom_line(linewidth = 0.7,
             linetype = "solid") +
   scale_x_discrete(breaks = c("Feb 2023", "Jun 2023", "Aug 2023", "Dec 2023", "Feb 2024", "May 2024", "Aug 2024")) +
@@ -403,60 +403,219 @@ supplier_costsgraph <- costs %>%
 wet_commodities_graph <- costs %>% 
   filter(Costs == "Wet Costs") %>%
   ggplot(aes(x = Pilot, y = Cost, size = Cost)) +
-  geom_point(aes(color = "red"), shape = 21, fill = "red") +
-  geom_text(aes(label = paste0("$", Cost)), size = 2, family = "opensans", fontface = "bold") +
+  geom_point(aes(color = "#3A1078"), shape = 21, fill = "#3A1078", stroke = 0) +
+  geom_text(aes(label = paste0("$",Cost), size = 6, family = "opensans", fontface = "bold"), color = "#E3F1FD") +
   labs(
     x = element_blank(),
     y = element_blank(),
-    title = "Number of schools receiving Wet Commodities",
+    title = "Total costs of delivering Wet Commodities to the schools (Daily)",
     subtitle = "",
     caption = "Source: Supplier Survey"
   ) +
+  annotate(
+    "curve",
+    x = 1,
+    xend = 1.75,
+    y = 48,
+    yend = 120,
+    color = "#3A1078",
+    curvature = -0.3,
+    arrow = arrow(type = "closed", length = unit(0.1, "inches"), ends = "first")) +
+  annotate(
+    "point",
+    x = 1.75,
+    y = 120,
+    color = "#3A1078",
+    fill = "#3A1078",
+    size = 2,
+    shape = 21) +
+  annotate(
+    "marquee",
+    x = 2.3,
+    y = 120,
+    label = "Commune-level suppliers also incur less costs per single supply of wet commodities (daily) compared to other supply modalities",
+    width = 0.3,
+    color = "#3A1078",
+    size = 4,
+    family = "opensans") +
   theme_minimal(base_size = 12, base_family = "opensans") +
   theme(
     legend.position = "none",
     axis.text.y = element_blank(),
-    axis.text.x = element_blank(),
+    axis.text.x = element_text(size = 15, family = "opensans", face = "bold", colour = "#3A1078"),
     panel.spacing = unit(0, "lines"),  # Narrow the gap between facets
     strip.text = element_text(face = "bold"),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
-    panel.grid.major.x = element_line(color = "grey", size = 0.5, linetype = "dashed"),
-    plot.caption = element_text(hjust = 0, size = 8,  family = "opensans"),
-    plot.caption.position = "plot"
-  ) +
+    panel.grid.major.x = element_line(color = "#D5C5EB", size = 0.5, linetype = "dashed"),
+    plot.caption = element_text(hjust = 0, size = 10,  family = "opensans", face = "bold", color = "#3A1078"),
+    plot.caption.position = "plot",
+    plot.title = element_text(size = 18, family = "opensans", face = "bold", hjust = 0.5, color = "#3A1078")) +
   scale_size_area(max_size = 25) +
-  coord_cartesian(ylim = c(0, 200))
+  coord_cartesian(ylim = c(0, 150))
+
+ggsave(
+  "report/wet_costsgraph.png",
+  plot = wet_commodities_graph,
+  width = 6.67, height = 4.63, dpi = 300,
+  bg = "#E3F1FD"
+)
 
 
 
 dry_commodities_graph <- costs %>% 
   filter(Costs == "Dry Costs") %>%
   ggplot(aes(x = Pilot, y = Cost, size = Cost)) +
-  geom_point(aes(color = "red"), shape = 21, fill = "red") +
-  geom_text(aes(label = paste0("$",Cost), size = 2, family = "opensans", fontface = "bold")) +
+  geom_point(aes(color = "#740938"), shape = 21, fill = "#740938", stroke =  0) +
+  geom_text(aes(label = paste0("$",Cost), size = 9, family = "opensans", fontface = "bold"), color = "#FFFFF2") +
   labs(
     x = element_blank(),
     y = element_blank(),
-    title = "Number of schools receiving Wet Commodities",
+    title = "Total costs of delivering Dry Commodities to the schools (Monthly)",
     subtitle = "",
     caption = "Source: Supplier Survey"
   ) +
+  annotate(
+    "segment",
+    x = 1.16,
+    xend = 1.75,
+    y = 120,
+    yend = 120,
+    color = "#740938",
+    linewidth = 0.5
+  ) + 
+  annotate(
+    "point",
+    x = 1.75,
+    y = 120,
+    color = "#740938",
+    fill = "#740938",
+    size = 2,
+    shape = 21) + 
+  annotate(
+    "point",
+    x = 1.16,
+    y = 120,
+    color = "#740938",
+    fill = "#740938",
+    size = 2,
+    shape = 21) +
+  annotate(
+    "marquee",
+    x = 2.3,
+    y = 120,
+    label = "Commune-level suppliers incur less costs per single supply of dry commodities (monthly) compared to District-level and non-pilot suppliers",
+    width = 0.3,
+    color = "#740938",
+    size = 4,
+    family = "opensans") +
   theme_minimal(base_size = 12, base_family = "opensans") +
   theme(
     legend.position = "none",
     axis.text.y = element_blank(),
-    #axis.text.x = element_blank(),
+    axis.text.x = element_text(size = 15, family = "opensans", face = "bold"),
     panel.spacing = unit(0, "lines"),  # Narrow the gap between facets
     strip.text = element_text(face = "bold"),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
     panel.grid.major.x = element_line(color = "grey", size = 0.5, linetype = "dashed"),
-    plot.caption = element_text(hjust = 0, size = 8,  family = "opensans"),
-    plot.caption.position = "plot"
-  ) +
+    plot.caption = element_text(hjust = 0, size = 10,  family = "opensans", face = "bold", color = "#740938"),
+    plot.caption.position = "plot",
+    plot.title = element_text(size = 18, family = "opensans", face = "bold", hjust = 0.5, color = "#740938")) +
   scale_size_area(max_size = 25) +
   coord_cartesian(ylim = c(0, 300))
+
+ggsave(
+  "report/dry_costsgraph.png",
+  plot = dry_commodities_graph,
+  width = 6.67, height = 4.63, dpi = 300,
+  bg = "#FFFFF2"
+)
+
+#############################################################################################################################
+
+# Cost Efficiency Graph - Evidence Generation
+
+WetCostsPerChildEG <- FullTablesData %>% 
+  filter(District != "Krakor") %>% 
+  group_by(SchoolId, procurement, Year) %>%
+  summarise(AvgStudents = mean(AvgStudents, na.rm = TRUE),
+            WetCostsPerChild = mean(WetCostsPerChild, na.rm = TRUE),
+            DryCostsPerChild = mean(DryCostsPerChild, na.rm = TRUE)) %>%
+  ungroup() %>%
+  filter(AvgStudents > 0 & AvgStudents < 500 ) %>%
+  filter(Year == 2024) %>% 
+  #filter(WetCostsPerChild > quantile(WetCostsPerChild, 0.05) & WetCostsPerChild < quantile(WetCostsPerChild, 0.95)) %>%
+  ggplot(aes(x = AvgStudents, y = WetCostsPerChild)) +
+  geom_point(position = position_jitter(width = 0.1, height = 0.1), colour = "#181C14", size = 0.8) + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = "#1230AE", linewidth = 0.5, fill = "#7F8FE5") + 
+  #scale_y_log10() +
+  facet_wrap(~ procurement, scales = "free") + 
+  theme_clean() +
+  coord_cartesian(ylim = c(1.5, 3)) +
+  labs(title = "Relationship between the monthly cost per child and the number of eating students (2024)",
+       x = "Number of Eating Students",
+       y = "Monthly Cost per Child (USD)",
+       caption = "Data Source: SFIS") +
+  theme(
+    plot.title = element_text(size = 16, family = "opensans", face = "bold", hjust = 0.5, lineheight = 2, color = "#1230AE"),
+    axis.title.y = element_text(size = 14, family = "opensans", face = "bold", margin = margin(l = 5, r = 5), color = "#1230AE"),
+    axis.title.x = element_text(size = 14, family = "opensans", face = "bold", margin = margin(t = 10, b = 5), color = "#1230AE"),
+    axis.text.x = element_text(family = "opensans", size = 12, color = "#1230AE", face = "bold"),
+    axis.line.x = element_line(color = "#1230AE"),
+    axis.line.y = element_line(color = "#1230AE"),
+    axis.ticks = element_line(color = "#1230AE"),
+    axis.text.y = element_text(family = "opensans", size = 10, color = "#1230AE", face = "bold"),
+    panel.background = element_rect(fill = "#FBFBFB", color = "#FBFBFB"),
+    plot.background = element_rect(fill = "#FBFBFB", color = "#FBFBFB"),
+    plot.title.position = "plot",
+    plot.caption = element_text(size = 10, hjust = 0, family = "opensans", face = "bold", color = "#1230AE"),
+    plot.caption.position = "plot",
+    panel.border = element_blank(),
+    strip.text = element_text(size = 12, face = "bold", family = "opensans", margin = margin(t = 10), color = "#1230AE"),
+    strip.background = element_rect(fill = "#FBFBFB"))
+
+# Save the graph
+ggsave("report/WetCostsPerChildEG.png", 
+       plot = WetCostsPerChildEG, 
+       width = 9.14, height = 5.36, dpi = 300)
+
+##############################################################################################################
+
+# Cost per child trend graph - Evidence Generation Workshop
+
+costs_trend_graphEG <- PanelData %>% 
+  filter(!MonthYear %in% c("Jan 2023", "Apr 2023", "May 2023", "Oct 2023", "Apr 2024")) %>%
+  ggplot(aes(x = MonthYear, y = AvgTotalCost, group = procurement, color = procurement)) +
+  geom_line(linewidth = 0.7, linetype = "solid") +
+  scale_x_discrete(breaks = c("Feb 2023", "Jun 2023", "Aug 2023", "Dec 2023", "Feb 2024", "May 2024", "Aug 2024"),
+                   expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  geom_point(size = 1) +
+  theme_clean() +
+  annotate(
+    geom = "rect",
+    xmin = 0, xmax = "Feb 2024",
+    ymin = 0, ymax = 9.4,
+    fill = "grey",
+    alpha = 0.2) +
+  labs(title = "Cost per Child over Time",
+       x = "Month",
+       y = "Cost per Child (USD)") +
+  theme(
+    plot.title = element_text(size = 7, face = "bold", lineheight = 2),
+    axis.title.y = element_text(size = 6, family = "opensans", face = "bold", margin = margin(l = 5, r = 5)),
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(family = "opensans", size = 5, color = "black"),
+    axis.text.y = element_text(family = "opensans", size = 5, color = "black"),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 4, family = "opensans", color = "black"),
+    legend.margin = margin(0, 0, 0, 0),
+    legend.box.spacing = unit(0, "cm"),
+    legend.position = "bottom",
+    legend.background = element_blank())
+
+
 
 
 
