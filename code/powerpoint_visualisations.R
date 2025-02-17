c377d8d4af496873f127caa8db25bbd2aafca49c
Eltone Mabodo <eltone.mabodo@gmail.com>
2025-02-17 08:09:23 +0700

    Initialising visualisations for the powerpoint presendation

---
+++ code/powerpoint_visualisations.R
@@ -0,0 +1,14 @@
+#Loading relevant libraries
+
+library(tidyverse)
+library(sf)
+library(showtext)
+
+
+########################################################################
+#Text for the graphs - setting the defalts
+font_add_google("Open Sans","opensans")
+font_add_google("EB Garamond","garamond")
+showtext_auto()
+showtext_opts(dpi = 200)
+########################################################################
